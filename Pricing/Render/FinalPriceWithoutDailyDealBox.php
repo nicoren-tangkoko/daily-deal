676932a3756ed1ea8a2d0e13f495e97c28dd98e0
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2019-01-04 13:12:29 +0000

    Change priceBox rule for configurable items

--- Pricing/Render/FinalPriceWithoutDailyDealBox.php
+++ Pricing/Render/FinalPriceWithoutDailyDealBox.php
@@ -9,6 +9,11 @@ class FinalPriceWithoutDailyDealBox extends \Magento\Catalog\Pricing\Render\Fina
     {
         if($priceCode == \Magento\Catalog\Pricing\Price\FinalPrice::PRICE_CODE){
             $priceCode = \MageSuite\DailyDeal\Pricing\Price\FinalPriceWithoutDailyDeal::PRICE_CODE;
+
+            if($this->saleableItem->getTypeId() == \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE){
+                $priceCode = \Magento\Catalog\Pricing\Price\RegularPrice::PRICE_CODE;
+            }
+
         }
 
         return $this->saleableItem->getPriceInfo()->getPrice($priceCode);
